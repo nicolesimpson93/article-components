e81d2bebcb6990c1d5d8bc9d086017f43c25e301
graphic <graphic@WFD228-36m.local>
2020-02-13 09:12:58 -0500

    Mostly solved scroll progress, part a

--- js/index.js
+++ js/index.js
@@ -4,33 +4,36 @@ let $win = window
 let $bar = document.querySelector('.progress-bar')
 
 
-// STEPS
-
-// Height of the window (without scroll bars)
-let winH = $doc.clientHeight 
-
-// Height of the document (html page)
-let docH = $doc.scrollHeight 
-
-// "None" the .progress-bar display
-$bar.style.display = 'none'
-
-// "Block" the .progress-bar display
-$bar.style.display = 'block'
-
-// if (docH is less than winH), hide the progress-bar
-
-// Determine how much CAN be scrolled
-let canBeScrolled = docH - winH
-
-// Determine how much HAS been scrolled
-let hasBeenScrolled = $win.scrollY
-
-// What percentage of the page has been scrolled
-let percentScrolled = hasBeenScrolled / canBeScrolled
-
-// Convert scrolled % out of 100 and apply to the width of .progress-bar
-$bar.style.width = `${percentScrolled * 100}%`
+// EXECUTION
 
 // Listen for the window to scroll
-$win.addEventListener('scroll', event => { console.log('scroll!!!') })
+$win.addEventListener('scroll', event => { 
+  
+  // Height of the window (without scroll bars)
+  let winH = $doc.clientHeight 
+
+  // Height of the document (html page)
+  let docH = $doc.scrollHeight 
+
+  // if (docH is less than winH), hide the progress-bar
+  if (docH <= winH) {
+    // "None" the .progress-bar display
+    $bar.style.display = 'none'
+  } else {
+    // "Block" the .progress-bar display
+    $bar.style.display = 'block'
+  }
+
+  // Determine how much CAN be scrolled
+  let canBeScrolled = docH - winH
+
+  // Determine how much HAS been scrolled
+  let hasBeenScrolled = $win.scrollY
+
+  // What percentage of the page has been scrolled
+  let percentScrolled = hasBeenScrolled / canBeScrolled
+
+  // Convert scrolled % out of 100 and apply to the width of .progress-bar
+  $bar.style.width = `${percentScrolled * 100}%`
+
+})
