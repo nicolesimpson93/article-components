ff777ca5648dfd86d3af08c73d4c25467a088424
Nicole Simpson <nicolesimpson@Nicoles-MacBook-Pro-2.local>
2020-02-13 13:05:52 -0500

    steps updated

--- js/index.js
+++ js/index.js
@@ -2,4 +2,33 @@
 let $doc = document.documentElement //htmlelement
 let $win = window
 let $bar = document.querySelector (`.progress-bar`)
+
+
 //Steps 
+//Height of the window 
+let winH = $doc.clientHeight
+
+//Height of the document 
+let docH = $doc.scrollHeight
+
+//Make  .progress- bar not displayed
+$bar.stlye.display = `none`
+
+//Make .progress- bar displayed
+$bar.stlye.display = `block`
+
+//If the $doc height < $win height...
+
+//Store hwo much HAS been scrolled 
+
+//Store how much CAN be scrolled
+
+//Calculater the  % scrolled of avilable 
+
+//Apply a % of the $bar width 
+
+//Capture the scroll of the window
+
+
+
+$bar.style.display
