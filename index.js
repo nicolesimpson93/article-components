ef78921f7e89af1041ade27d045487a19d1ad8d1
Nicole Simpson <nicolesimpson@Nicoles-MacBook-Pro-2.local>
2020-02-13 14:34:27 -0500

    steps so far

--- index.js
+++ index.js
@@ -3,3 +3,23 @@ let $doc = document.documentElement //htmlelement
 let $win = window
 let $bar = document.querySelector (`.progress-bar`)
 
+let $doc = document. documentElement //html element 
+let $win = window 
+let $bar = document. querySelector(`bar`)
+let $prog = document. querySelector(`progress`)
+let $h1 =  document. querySelector(`#heading`)
+let $h1copy = document.querySelector(`h1copy`)
+
+
+
+
+//STEPS
+
+//Store the textContent from the h1 
+
+//Apply new textContent to the .h1copy
+
+//apply opacity: 0 to h1copy (decimal number but represents a percentage)
+
+//Determine when h1 isoff the top of the window 
+    //A) Manually scroll, store the window. scrollY
